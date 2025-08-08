3b3d3a92c076fb8f3bf94d277280a4da9de621ed
João Vitor Carvalho Barbosa <joaovitorcarvalho903@gmail.com>
2025-08-08 17:26:54 -0300

    feat: adiconando salvar carro

--- clientes/views.py
+++ clientes/views.py
@@ -3,9 +3,11 @@ from django.http import HttpResponse
 from .models import Cliente, Carro
 import re
 
+TEMPLATE_CLIENTES = 'clientes.html'
+
 def clientes(request):
     if request.method == 'GET':
-        return render(request, 'clientes.html')
+        return render(request, TEMPLATE_CLIENTES)
     elif request.method == 'POST':
         nome = request.POST.get('nome')
         sobrenome = request.POST.get('sobrenome')
@@ -14,6 +16,18 @@ def clientes(request):
         carros= request.POST.getlist('carro')
         placas= request.POST.getlist('placa')
         anos= request.POST.getlist('ano')
+
+        cliente = Cliente.objects.filter(cpf=cpf)
+
+
+
+        if cliente.exists():
+            return render(request, TEMPLATE_CLIENTES, {'nome': nome, 'sobrenome': sobrenome, 'email': email, 'cpf': cpf, 'carros': zip(carros,placas,anos)})
+           
+
+        if not re.fullmatch(re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+'), email):
+            return render(request, TEMPLATE_CLIENTES, {'nome': nome, 'sobrenome': sobrenome, 'cpf': cpf, 'carros': zip(carros,placas,anos)})
+
 
         cliente= Cliente(
             nome = nome,
